468b53abbda0ee8ca4981899f9bd2e96ce75c35c
GJQKP <126954358+GJQKP@users.noreply.github.com>
2023-03-25 12:21:18 +0100

    Update display.js

--- util/display.js
+++ util/display.js
@@ -25,7 +25,7 @@ const home = {
 			},
 		},
 	},
-	author: 'Rizky Aulia Tumangger | @GJQKP | iyaddz[at]iyaddz.ml',
+	author: 'Iyad Belamri | @GJQKP | iyaddz[at]iyaddz.ml',
 	docs: 'https://t.me/qrapi1',
 };
 
